82bf6dc9e530caa1ae9118308c9977d9beeb1001
Srikanth Bandi <99063718+srikanthbandi949@users.noreply.github.com>
2022-03-11 11:05:32 +0530

    Add files via upload

---
+++ Day_1 Activity/StringUpper.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class StringUpper {
+ 
+    public static void main(String [] args){
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter First name : ");
+        String input1 = s.nextLine();
+        System.out.println("Enter Last name : ");
+        String input2 = s.nextLine();
+
+        String first_letter = input1.substring(0,1);
+        String remaining_letters = input1.substring(1);
+
+        System.out.println(first_letter.toUpperCase()+remaining_letters.toLowerCase()+" "+input2.toUpperCase());
+
+        s.close();
+    }
+}
